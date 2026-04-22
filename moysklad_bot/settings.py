f2a094025739a97139b2ec9be456eea6fd32765c
mukhamadovv <m.mukhamadovv@gmail.com>
2026-04-23 00:19:48 +0500

    Use DATABASE_URL for database configuration
    Replaced hardcoded database settings with environment variable.

--- moysklad_bot/settings.py
+++ moysklad_bot/settings.py
@@ -89,14 +89,7 @@ WSGI_APPLICATION = 'moysklad_bot.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'uhMLbDsToPCYUNvcvMfgWSkKMirrZDka',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # ─── Static ───────────────────────────────────────────
