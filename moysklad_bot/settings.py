a48248a68de067ed7f52671ac3a01c9bb376486a
mukhamad0v <straxpubgm777@gmail.com>
2026-04-24 00:42:30 +0500

    change 6

--- moysklad_bot/settings.py
+++ moysklad_bot/settings.py
@@ -89,10 +89,7 @@ WSGI_APPLICATION = 'moysklad_bot.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # ─── Static ───────────────────────────────────────────
