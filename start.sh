8265302bdd7c347d67f868dee330663eb1442b18
mukhamad0v <straxpubgm777@gmail.com>
2026-04-23 19:47:41 +0500

    correcting 3

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=1
 
-# Run sync_transactions in the background so gunicorn starts immediately
-python -u manage.py sync_transactions &
+# Run sync_transactions in foreground first so we can see all output/errors
+echo "=== Starting sync_transactions ==="
+python -u manage.py sync_transactions
+echo "=== sync_transactions finished ==="
 
 # Start the web server
 exec gunicorn -c gunicorn.conf.py moysklad_bot.wsgi:application
